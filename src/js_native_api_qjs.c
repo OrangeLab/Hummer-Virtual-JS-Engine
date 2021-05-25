85847acec49220471ae91d9a78801589e448e20c
ChasonTang <chasontang@warmbloom.com>
2021-05-26 02:58:58 +0800

    fix: clang static analyzer error
    1. Extract convertUTF16toUTF8
    2. napi_create_symbol

--- src/js_native_api_qjs.c
+++ src/js_native_api_qjs.c
@@ -104,21 +104,22 @@ NAPIStatus setLastErrorCode(NAPIEnv env, NAPIStatus errorCode)
     CHECK_ENV(env);
 
     env->lastError.errorCode = errorCode;
-#ifndef NDEBUG
-    if (errorCode == NAPIPendingException)
-    {
-        CHECK_ARG(env, env->context);
-        JSValue exceptionValue = JS_GetException(env->context);
-        if (JS_IsNull(exceptionValue))
-        {
-            assert(false);
-        }
-        else
-        {
-            JS_Throw(env->context, exceptionValue);
-        }
-    }
-#endif
+    // assert 可能导致静态分析出现假阳性
+    //#ifndef NDEBUG
+    //    if (errorCode == NAPIPendingException)
+    //    {
+    //        CHECK_ARG(env, env->context);
+    //        JSValue exceptionValue = JS_GetException(env->context);
+    //        if (JS_IsNull(exceptionValue))
+    //        {
+    //            assert(false);
+    //        }
+    //        else
+    //        {
+    //            JS_Throw(env->context, exceptionValue);
+    //        }
+    //    }
+    //#endif
 
     return errorCode;
 }
@@ -454,6 +455,131 @@ NAPIStatus napi_create_string_utf8(NAPIEnv env, const char *str, size_t length, 
     return clearLastError(env);
 }
 
+typedef enum
+{
+    ConversionOK,    /* conversion successful */
+    SourceExhausted, /* partial character in source, but hit end */
+    TargetExhausted, /* inSufficent. room in target for conversion */
+    SourceIllegal    /* source sequence is illegal/malformed */
+} ConversionResult;
+
+static const uint32_t UNI_SUR_HIGH_START = 0xd800;
+
+static const uint32_t UNI_SUR_HIGH_END = 0xdbff;
+
+static const uint32_t UNI_SUR_LOW_START = 0xdc00;
+
+static const uint32_t UNI_SUR_LOW_END = 0xdfff;
+
+static const uint8_t halfShift = 10;
+
+static const uint32_t halfBase = 0x10000;
+
+static const uint32_t UNI_REPLACEMENT_CHAR = 0x0000fffd;
+
+/*
+ * Once the bits are split out into bytes of UTF-8, this is a mask OR-ed
+ * into the first byte, depending on how many bytes follow.  There are
+ * as many entries in this table as there are UTF-8 sequence types.
+ * (I.e., one byte sequence, two byte... etc.). Remember that sequencs
+ * for *legal* UTF-8 will be 4 or fewer bytes total.
+ */
+static const uint8_t firstByteMark[7] = {0x0, 0x0, 0xc0, 0xe0, 0xf0};
+
+static ConversionResult convertUTF16toUTF8(const uint16_t *sourceStart, const uint16_t *sourceEnd,
+                                           uint8_t **targetStart, const uint8_t *targetEnd)
+{
+    ConversionResult result = ConversionOK;
+    uint8_t *target = *targetStart;
+    while (sourceStart <= sourceEnd)
+    {
+        uint32_t unicodeValue;
+        // 无效为 0xfffd，3 字节
+        uint8_t bytesToWrite = 3;
+        const uint32_t byteMask = 0xBF;
+        const uint32_t byteMark = 0x80;
+        unicodeValue = *sourceStart++;
+        if (unicodeValue >= UNI_SUR_HIGH_START && unicodeValue <= UNI_SUR_HIGH_END)
+        {
+            // 代理平面
+            if (sourceStart < sourceEnd)
+            {
+                uint16_t tail = *sourceStart;
+                if (tail >= UNI_SUR_LOW_START && tail <= UNI_SUR_LOW_END)
+                {
+                    unicodeValue =
+                        ((unicodeValue - UNI_SUR_HIGH_START) << halfShift) + (tail - UNI_SUR_LOW_START) + halfBase;
+                    ++sourceStart;
+                }
+                else
+                {
+                    result = SourceIllegal;
+
+                    break;
+                }
+            }
+            else
+            {
+                result = SourceExhausted;
+
+                break;
+            }
+        }
+        else
+        {
+            /* UTF-16 surrogate values are illegal in UTF-32 */
+            if (unicodeValue >= UNI_SUR_LOW_START && unicodeValue <= UNI_SUR_LOW_END)
+            {
+                result = SourceIllegal;
+
+                break;
+            }
+        }
+        /* Figure out how many bytes the result will require */
+        if (unicodeValue < (uint32_t)0x80)
+        {
+            bytesToWrite = 1;
+        }
+        else if (unicodeValue < (uint32_t)0x800)
+        {
+            bytesToWrite = 2;
+        }
+        else if (unicodeValue < (uint32_t)0x10000)
+        {
+            bytesToWrite = 3;
+        }
+        else if (unicodeValue < (uint32_t)0x110000)
+        {
+            bytesToWrite = 4;
+        }
+        else
+        {
+            //            bytesToWrite = 3;
+            unicodeValue = UNI_REPLACEMENT_CHAR;
+        }
+
+        if (target + bytesToWrite - 1 > targetEnd)
+        {
+            //            target -= bytesToWrite - 1;
+            result = TargetExhausted;
+
+            break;
+        }
+        // 从后往前写
+        // offset 为 3，2，1，0
+        // 但是 0 不考虑在循环做处理
+        for (uint8_t offset = bytesToWrite - 1; offset > 0; --offset)
+        {
+            *(target + offset) = (uint8_t)((unicodeValue | byteMark) & byteMask);
+        }
+        *target = (uint8_t)(unicodeValue | firstByteMark[bytesToWrite]);
+        target += bytesToWrite;
+    }
+    *targetStart = target;
+
+    return result;
+}
+
 // V8 引擎传入 NULL 返回 ""
 // NAPIPendingException/NAPIMemoryError/NAPIGenericFailure + addValueToHandleScope
 NAPIStatus napi_create_string_utf16(NAPIEnv env, const char16_t *str, size_t length, NAPIValue *result)
@@ -497,7 +623,7 @@ NAPIStatus napi_create_string_utf16(NAPIEnv env, const char16_t *str, size_t len
         }
         // 这里不需要考虑 \0
         // 0x0 -> 0x10ffff UTF-32 UTF-16 包含的 Unicode，UTF-8 正常应当可以扩展至 6 字节
-        utf8String = malloc(sizeof(char) * 4 * length);
+        utf8String = malloc(sizeof(uint8_t) * 4 * length);
         if (!utf8String)
         {
             if (*((uint8_t *)&value) == 1)
@@ -509,96 +635,24 @@ NAPIStatus napi_create_string_utf16(NAPIEnv env, const char16_t *str, size_t len
             return setLastErrorCode(env, NAPIMemoryError);
         }
         // UTF-16 -> UTF-8
-        int i = 0;
-        int j = 0;
-        uint32_t unicodeValue = 0;
-        while (i < length)
-        {
-            if (str[i] >= 0xd800 && str[i] <= 0xdbff)
-            {
-                // 代理平面
-                if (i + 1 == length)
-                {
-                    // 只有 head，缺少 trail，UTF-16 字符串不完整
-                    free(utf8String);
-                    if (*((uint8_t *)&value) == 1)
-                    {
-                        // 大端
-                        free((void *)str);
-                    }
-
-                    return setLastErrorCode(env, NAPIGenericFailure);
-                }
-                uint16_t head = str[i++] & 0x400;
-                // 读取 tail
-                uint16_t trail = str[i++];
-                if (trail < 0xdc00 || trail > 0xdfff)
-                {
-                    // tail 非法
-                    free(utf8String);
-                    if (*((uint8_t *)&value) == 1)
-                    {
-                        // 大端
-                        free((void *)str);
-                    }
-
-                    return setLastErrorCode(env, NAPIGenericFailure);
-                }
-                trail = trail & 0x400;
-                unicodeValue = ((uint32_t)head) << 10 | trail | 0x10000;
-            }
-            else
-            {
-                // Unicode
-                unicodeValue = str[i++];
-            }
-            if (unicodeValue < 0x80)
-            {
-                // 0xxxxxxx -> 7
-                utf8String[j++] = unicodeValue;
-            }
-            else if (unicodeValue < 0x800)
-            {
-                // 110xxxxx 10xxxxxx -> 11
-                utf8String[j++] = (0x7c0 & unicodeValue) >> 6 | 0xc0;
-                utf8String[j++] = 0x3f & unicodeValue | 0x80;
-            }
-            else if (unicodeValue < 0x10000)
-            {
-                // 1110xxxx 10xxxxxx 10xxxxxx
-                utf8String[j++] = (0xf000 & unicodeValue) >> 12 | 0xe0;
-                utf8String[j++] = (0xfc0 & unicodeValue) >> 6 | 0x80;
-                utf8String[j++] = 0x3f & unicodeValue | 0x80;
-            }
-            else if (unicodeValue < 0x110000)
-            {
-                // 11110xxx 10xxxxxx 10xxxxxx 10xxxxxx
-                utf8String[j++] = (0x1c0000 & unicodeValue) >> 18 | 0xf0;
-                utf8String[j++] = (0x3f000 & unicodeValue) >> 12 | 0x80;
-                utf8String[j++] = (0xfc0 & unicodeValue) >> 6 | 0x80;
-                utf8String[j++] = 0x3f & unicodeValue | 0x80;
-            }
-            else
-            {
-                free(utf8String);
-                if (*((uint8_t *)&value) == 1)
-                {
-                    // 大端
-                    free((void *)str);
-                }
-
-                return setLastErrorCode(env, NAPIGenericFailure);
-            }
-        }
-
+        uint8_t *destination = utf8String;
+        ConversionResult conversionResult =
+            convertUTF16toUTF8(str, str + length - 1, &destination, destination + length - 1);
         if (*((uint8_t *)&value) == 1)
         {
             // 大端
             free((void *)str);
         }
-    }
+        if (conversionResult == SourceIllegal || conversionResult == SourceExhausted)
+        {
+            free(utf8String);
 
-    JSValue stringValue = JS_NewStringLen(env->context, (char *)utf8String, utf8StringLength);
+            return setLastErrorCode(env, NAPIGenericFailure);
+        }
+        // 部分转换也算转换成功
+        utf8StringLength = destination - utf8String;
+    }
+    JSValue stringValue = JS_NewStringLen(env->context, (const char *)utf8String, utf8StringLength);
     free(utf8String);
     RETURN_STATUS_IF_FALSE(env, !JS_IsException(stringValue), NAPIPendingException);
     struct Handle *stringHandle;
@@ -607,6 +661,30 @@ NAPIStatus napi_create_string_utf16(NAPIEnv env, const char16_t *str, size_t len
 
     return clearLastError(env);
 }
+
+// napi_get_global + napi_get_named_property + napi_call_function
+NAPIStatus napi_create_symbol(NAPIEnv env, NAPIValue description, NAPIValue *result)
+{
+    CHECK_ENV(env);
+    CHECK_ARG(env, result);
+
+    NAPIValue global, symbolFunc;
+    CHECK_NAPI(napi_get_global(env, &global));
+    CHECK_NAPI(napi_get_named_property(env, global, "Symbol", &symbolFunc));
+    CHECK_NAPI(napi_call_function(env, global, symbolFunc, 1, &description, result));
+
+    return clearLastError(env);
+}
+
+// NAPIStatus napi_create_function(NAPIEnv env, const char *utf8name, size_t length, NAPICallback cb, void *data,
+//                                 NAPIValue *result)
+//{
+//     CHECK_ENV(env);
+//     CHECK_ARG(env, result);
+//     CHECK_ARG(env, cb);
+//
+//     return clearLastError(env);
+// }
 
 NAPIStatus napi_typeof(NAPIEnv env, NAPIValue value, NAPIValueType *result)
 {
