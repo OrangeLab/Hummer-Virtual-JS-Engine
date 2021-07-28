8df116e6ce0482cb393fb5ad892eeb574f06c961
ChasonTang <chasontang@warmbloom.com>
2021-07-28 19:50:47 +0800

    refactor(Hermes): Optimize logging.h implementation
    1. Fix Android Hermes build

--- third_party/include/glog/logging.h
+++ third_party/include/glog/logging.h
@@ -3,18 +3,26 @@
 #define INFO 0
 #define WARNING 0
 
-#define LOG(severity) ::std::cout
+#define LOG(severity)                                                                                                  \
+    if (0)                                                                                                             \
+    ::std::cout
 
-#define CHECK(condition) ::std::cout
+#define CHECK(condition)                                                                                               \
+    if (0)                                                                                                             \
+    ::std::cout
 #define CHECK_GE(val1, val2)
 #define CHECK_EQ(val1, val2)
 
 #define DCHECK(condition)
-#define DCHECK_EQ(val1, val2) ::std::cout
+#define DCHECK_EQ(val1, val2)                                                                                          \
+    if (0)                                                                                                             \
+    ::std::cout
 #define DCHECK_LE(val1, val2)
 #define DCHECK_GT(val1, val2)
 #define DCHECK_LT(val1, val2)
 #define DCHECK_NE(val1, val2)
 #define DCHECK_GE(val1, val2)
 
-#define LOG_FIRST_N(severity, n) ::std::cout
+#define LOG_FIRST_N(severity, n)                                                                                       \
+    if (0)                                                                                                             \
+    ::std::cout
