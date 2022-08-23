dc35fa2d7e19d7d59334912b2a4b2fa9ffb60430
Chason Tang <tangjiacheng@acornsemi.com>
2022-08-23 22:13:39 +0800

    feat: Add jsi_dynamic jsinspector double_conversion fmt folly.
    1. Modify .gitignore file.

--- third_party/include/glog/logging.h
+++ third_party/include/glog/logging.h
@@ -3,18 +3,16 @@
 #define INFO
 #define WARNING
 
-#define LOG(severity)                                                                                                  \
-    if (0)                                                                                                             \
-    ::std::cout
+#define LOG(severity) ::std::cout
 
-#define CHECK(condition)
+#define CHECK(condition) ::std::cout
 #define CHECK_EQ(val1, val2)
+#define CHECK_GE(val1, val2)
 
 #define DCHECK(condition)
-#define DCHECK_EQ(val1, val2)                                                                                          \
-    if (0)                                                                                                             \
-    ::std::cout
+#define DCHECK_EQ(val1, val2) ::std::cout
 #define DCHECK_GT(val1, val2)
 #define DCHECK_LT(val1, val2)
 #define DCHECK_NE(val1, val2)
 #define DCHECK_GE(val1, val2)
+#define DCHECK_LE(val1, val2)
